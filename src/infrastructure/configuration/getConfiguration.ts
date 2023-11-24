0cf3a1c0cd0b6d89a515af39085829cc9a4a3130
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-11-25 00:55:09 +0500

    updated redirect uri configuration

--- src/infrastructure/configuration/getConfiguration.ts
+++ src/infrastructure/configuration/getConfiguration.ts
@@ -5,7 +5,7 @@ export const getConfiguration = () => ({
   client_secret:
     'FNQ64wwWzqp2pW3mrIFDtACWrrRuqLoBY1uM716PwjeJbnk3D4tNq0D9Ax7d1bmq', // Секретный ключ
   redirect_uri:
-    'https://7a34-2a0d-5600-24-1162-3455-e251-e75c-44.ngrok-free.app/auth/callback', // ссылка для перенаправления
+    'https://amo-crm-contact-deals-service.vercel.app/auth/callback', // ссылка для перенаправления
 });
 
 export type ConfigType = ReturnType<typeof getConfiguration>;
